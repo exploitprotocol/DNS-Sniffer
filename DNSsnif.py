2768391216d76c037574d393a5ad65a93d01186c
Aditya Agrawal <AdityaAgrawal@users.noreply.github.com>
2013-11-24 10:01:01 +0530

    Update and rename DNSnif.py to DNSsnif.py

--- DNSsnif.py
+++ DNSsnif.py
@@ -1,6 +1,4 @@
-
-
-
+#!usr/bin/python
 """ DNSnif.py-It basically captures all the DNS Queries from the traffic and It can be used 
     with an arp poisioning tool to capture the all the DNS Queries over the network
     
@@ -9,8 +7,8 @@
     Author is not responsible for any effect's caused by this script
 """
 
-
 from scapy.all import *
+
 serverip=raw_input("Input IP Address of DNS Server : ")
 result={}
 def packetHandler(pkt):
@@ -27,7 +25,7 @@ def printResult():
 def liveView(pkt):
 	if pkt.haslayer(DNSQR):
 		print pkt[DNSQR].qname[0:-1]
-view=raw_input("Enter s if you want to store then show result's and l for liveview")
+view=raw_input("Enter s if you want to store then show result's and l for liveview : ")
 if view=='s':
 	query=raw_input("Input No. Of queries to be sniffed : ")
 	sniff(count=int(query),filter="udp port 53 and ip src "+str(serverip),prn=packetHandler)
